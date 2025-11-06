aae8519573171a223cd59460609c24cabd51064f
kauanallyson <163926307+kauanallyson@users.noreply.github.com>
2025-11-06 01:12:47 -0300

    feat: adiciona animacoes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  srcDir: 'src',
-}
+  reactCompiler: false,
+};
 
-export default nextConfig
+export default nextConfig;
